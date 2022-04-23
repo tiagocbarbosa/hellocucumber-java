bf180464aca848e287b5bf44d44487d51bcc6675
tiagocbarbosa <tiagocastrobarbosa@gmail.com>
2022-04-23 17:22:42 -0300

    "See scenario reported as passing" module done

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class IsItFriday {
     static String isItFriday(String today) {
-        return null;
+        return "Nope";
     }
 }
 
